39c771b413a69a8047871808abe56834be6d94d2
pooyax14 <pooyax14@gmail.com>
2019-10-24 15:35:14 +0530

    Feat(): Fetch markdown data as html using graphal query
    - install gatsby-transformer-remark to convert md to HTML
    - using graphQL query to get the markdownRemark nodes
    - add gatsby-transformer-remark package in gatsby config

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,38 @@
 import React from "react";
 import Layout from '../components/layout';
+import { graphql, useStaticQuery} from 'gatsby'
 
 const Projects = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark{
+        edges {
+          node{
+            frontmatter{
+              title,
+              date
+            }
+            
+          }
+        }
+      }
+    }
+  `)
+  const titleDate = data.allMarkdownRemark.edges.map((val, index) => {
+    return (
+      <li key={index}>
+        <h2>{val.node.frontmatter.title}</h2>
+        <p>{val.node.frontmatter.date}</p>
+      </li>
+    )
+  })
   return (
     <Layout>
       <h1>Projects</h1>
       <p>These are some of the projects that I worked on in my free time.</p>
+      <ol>
+        {titleDate}
+      </ol>
     </Layout>
   )
 }
